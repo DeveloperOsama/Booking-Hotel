c90eaf97367e509de6225a9420c89325c1ef61a2
osamaDeveloper <developerosama66@gmail.com>
2024-09-07 20:22:28 +0300

    final-commit

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -5,20 +5,21 @@
         <div class="sidebar-header d-flex align-items-center">
             <div class="avatar"><img src="admin/img/avatar-6.jpg" alt="..." class="img-fluid rounded-circle"></div>
             <div class="title">
-                <h1 class="h5">Mark Stephen</h1>
-                <p>Web Designer</p>
+                <h1 class="h5">Osama Algahafi</h1>
+                <p>Backend</p>
             </div>
         </div>
         <!-- Sidebar Navidation Menus--><span class="heading">Main</span>
         <ul class="list-unstyled">
-            <li class="active"><a href="{{ route('home2') }}"> <i class="icon-home"></i>Home </a></li>
+            <li class="active"><a href="{{ route('home') }}"> <i class="icon-home"></i>Home </a></li>
+            <li class="active"><a href="{{ route('home2') }}"> <i class="icon-home"></i>View Site</a></li>
 
             <li><a href="#exampledropdownDropdown" aria-expanded="false" data-toggle="collapse">
                     <i class="icon-windows"></i>Hotel Rooms </a>
                 <ul id="exampledropdownDropdown" class="collapse list-unstyled ">
                     <li><a href="{{ route('create_room') }}">Add Room</a></li>
                     <li><a href="{{ route('view_room') }}">View Rooms</a></li>
-                    <li><a href="#">Page</a></li>
+
                 </ul>
             </li>
             <li>
--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -13,12 +13,11 @@
              <div class="col-md-4">
                 <h3>Menu Link</h3>
                 <ul class="link_menu">
-                   <li class="active"><a href="#">Home</a></li>
-                   <li><a href="about.html"> about</a></li>
-                   <li><a href="room.html">Our Room</a></li>
-                   <li><a href="gallery.html">Gallery</a></li>
-                   <li><a href="blog.html">Blog</a></li>
-                   <li><a href="contact.html">Contact Us</a></li>
+                   <li class="active"><a href="{{ route('home2') }}">Home</a></li>
+                   <li><a href="{{ route('hotel_about') }}"> about</a></li>
+                   <li><a href="{{ route('our_rooms') }}">Our Room</a></li>
+                   <li><a href="{{ route('hotel_gallary') }}">Gallery</a></li>
+                   <li><a href="{{ route('contact_us') }}">Contact Us</a></li>
                 </ul>
              </div>
              <div class="col-md-4">
